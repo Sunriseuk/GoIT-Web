90eddfa8169ce10c5851af74e346decb71e6152f
Mykola Prystash <109032191+Sunriseuk@users.noreply.github.com>
2022-12-29 20:15:38 +0200

    HW5

--- HW5/Final/main.py
+++ HW5/Final/main.py
@@ -27,12 +27,23 @@ def get_currencies_data(data: dict, searched_currencies: list[str] = ('USD', 'EU
     return {data['date']: all_currencies_info}
 
 
-def write_in_json(data: dict) -> None:
-    json_data: list = json.load(open('data.json'))
-    json_data.append(data)
+# def write_in_json(data: dict) -> None:
+#     json_data: list = json.load(open('data.json'))
+#     json_data.append(data)
     
+#     with open('data.json', 'w') as file:
+#         json.dump(data, file, indent=2)
+
+
+def write_in_json(data: dict) -> None:
+    if os.path.exists('Final/data.json'):
+        json_data: list = json.load(open('Final/data.json'))
+        json_data.append(data)
+    else:
+        json_data: list = [data]
+
     with open('data.json', 'w') as file:
-        json.dump(data, file, indent=2)
+        json.dump(json_data, file, indent=2)
 
 
 
